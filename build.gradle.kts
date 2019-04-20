2a21be35b817bcb151966e20cc23c783c63ecc07
Mikhail Brunmayer <mikhail.brunmayer@rtlabs.ru>
2019-04-20 12:01:27 +0300

    hello component

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    base
+    id("com.github.node-gradle.node") version "1.3.0"
+}
+
+node {
+    download = true
+}
+
+val assembleKotlinReact by tasks.registering(NpmTask::class) {
+    dependsOn(tasks.getByName("npmInstall"))
+    setArgs(listOf("run", "build"))
+}
+
+tasks.getByName("build").dependsOn(assembleKotlinReact)
--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "Жмякни кнопулю для приветствия..."
+        };
+        this.greet = this.greet.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <button onClick={this.greet}>Поприветствовать</button>
+                </div>
+                <p>{this.state.message}</p>
+            </div>
+        );
+    }
+
+    greet() {
+        console.log("Link was clicked");
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    this.setState({message: JSON.parse(request.responseText).message});
+                } else {
+                    console.log(`Request has failed with code ${request.status}`);
+                }
+            }
+        };
+        request.open("GET", "/greet", true);
+        request.send();
+    }
 }
 
 export default App;
